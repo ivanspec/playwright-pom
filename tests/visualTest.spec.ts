7145e13b0253596442a009b0f35807519cecb548
ivanspec <44164427+ivanspec@users.noreply.github.com>
2025-06-08 14:42:13 +0700

    fix: delete waitForTimeout after assert visualTesting

--- tests/visualTest.spec.ts
+++ tests/visualTest.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('user visual testing', async ({ page }) => {
+test('Example visual testing', async ({ page }) => {
   // Navigate to Google
   await page.goto('https://shopee.co.id/buyer/login?');
 
   await expect(page.getByRole('textbox', { name: 'No. Handphone/Username/Email' })).toBeVisible();
   await expect(page).toHaveScreenshot('homepage.png');
-  await page.waitForTimeout(3000);
   
 });
